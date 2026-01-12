353a87518610b644e11cf0d6e8ca312fe3626788
Gaute945 <mrboobai@proton.me>
2026-01-12 23:03:29 +0100

    update

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
@@ -41,14 +42,14 @@ type ContactDetails struct { // issue with types, num saved as string and insecu
 	Grape       string
 	Origin      string
 	Producer    string
-	Vintage     string
+	Vintage     int
 	Taste       string
 	Color       string
 	Aroma       string
-	Acidity     string
-	Sweetness   string
-	Price       string
-	ISWineScale string
+	Acidity     float64
+	Sweetness   float64
+	Price       float64
+	ISWineScale float64
 }
 
 func main() {
@@ -77,8 +78,9 @@ func main() {
 	pingErr := db.Ping()
 	if pingErr != nil {
 		log.Fatal(pingErr)
+	} else {
+		fmt.Println("Connected to db")
 	}
-	fmt.Println("Connected to db")
 
 	rows, err := db.Query("SELECT Id, Title, Grape, Origin, Producer, Vintage, Taste, Color, Aroma, Acidity, Sweetness, Price, ISwinescale FROM wines")
 	defer rows.Close()
@@ -123,28 +125,93 @@ func main() {
 			return
 		}
 
+		// sanetize/fix input
+		// strconv.Atoi can't take empty strings
+
+		func emptyString(string name)  {
+			if (r.FormValue(name) = "") {
+				r.FormValue(name) = NULL
+			}
+			return r.FormValue(name)
+		}
+
+		Vintage, err := strconv.Atoi()
+		if err != nil {
+			log.Fatal(err)
+		}
+		Acidity, err := strconv.Atoi(r.FormValue("acidity"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		Sweetness, err := strconv.Atoi(r.FormValue("sweetness"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		Price, err := strconv.Atoi(r.FormValue("price"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		ISWineScale, err := strconv.Atoi(r.FormValue("isWineScale"))
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		details := ContactDetails{
 			Title:       r.FormValue("title"),
 			Grape:       r.FormValue("grape"),
 			Origin:      r.FormValue("origin"),
 			Producer:    r.FormValue("producer"),
-			Vintage:     r.FormValue("vintage"),
+			Vintage:     Vintage,
 			Taste:       r.FormValue("taste"),
 			Color:       r.FormValue("color"),
 			Aroma:       r.FormValue("aroma"),
-			Acidity:     r.FormValue("acidity"),
-			Sweetness:   r.FormValue("sweetness"),
-			Price:       r.FormValue("price"),
-			ISWineScale: r.FormValue("isWineScale"),
+			Acidity:     float64(Acidity),
+			Sweetness:   float64(Sweetness),
+			Price:       float64(Price),
+			ISWineScale: float64(ISWineScale),
 		}
 
 		// do something with details
+		// Inserts our data into the users table and returns with the result and a possible error.
+		// The result contains information about the last inserted id (which was auto-generated for us) and the count of rows this query affected.
 
-		fmt.Print(details)
+		// result, err := db.Exec(`
+		// INSERT INTO users (
+		// username,
+		// password,
+		// created_at
+		// ) VALUES (?, ?, ?)`,
+		// username,
+		// password,
+		// createdAt)
+
+		result, err := db.Exec(`
+		INSERT INTO wines (Title, Grape, Origin, Producer, Vintage, Taste, Color, Aroma, Acidity, Sweetness, Price, ISWineScale
+		) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+			details.Title,
+			details.Grape,
+			details.Origin,
+			details.Producer,
+			details.Vintage,
+			details.Taste,
+			details.Color,
+			details.Aroma,
+			details.Acidity,
+			details.Sweetness,
+			details.Price,
+			details.ISWineScale)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		id, err := result.LastInsertId()
+		fmt.Println(id)
 
 		formtmpl.Execute(w, struct{ Success bool }{true})
 	})
 
-	fmt.Printf("Listing on port 8080")
-	http.ListenAndServe(":8080", nil)
+	port := "8080"
+
+	fmt.Printf("Listing on port " + port)
+	http.ListenAndServe(":"+port, nil)
 }
